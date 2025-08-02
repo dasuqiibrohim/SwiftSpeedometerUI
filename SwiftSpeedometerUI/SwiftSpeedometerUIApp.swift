9fd8f09962ddf49653a3593809974eaa0914fc90
Ibrohim Dasuqi <ibrohim.dasuqi@axa-mandiri.co.id>
2025-08-02 17:33:36 +0700

    first commit

---
+++ SwiftSpeedometerUI/SwiftSpeedometerUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSpeedometerUIApp.swift
+//  SwiftSpeedometerUI
+//
+//  Created by Ibrohim Dasuqi on 02/08/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSpeedometerUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
